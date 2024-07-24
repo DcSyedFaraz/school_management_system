23420ecaec66190a1f7fcf7953dda501a6d61c8b
alishashamoun <alishashamoun12345@gmail.com>
2024-07-24 20:30:41 +0500

    migration files create

---
+++ database/migrations/2024_07_24_150012_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->integer('userId');
+            $table->string('userName');
+            $table->string('email');
+            $table->string('mobile');
+            $table->string('password');
+            $table->string('token');
+            $table->string('userType');
+            $table->integer('schoolId')->nullable();
+            $table->string('regionId')->nullable();
+            $table->string('districtId')->nullable();
+            $table->string('wardId')->nullable();
+            $table->string('registrationNumber')->nullable();
+            $table->string('isActive')->default(1);
+            $table->string('isDeleted')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
