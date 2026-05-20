b446e814df79faf7d4794abd4e8a4b142954fc59
dcsyedfaraz <dcsyedfaraz@gmail.com>
2026-05-20 21:53:50 +0500

    change

--- config/ranks.php
+++ config/ranks.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    ['rankName' => 'A', 'rankRangeMin' => 40, 'rankRangeMax' => 50],
-    ['rankName' => 'B', 'rankRangeMin' => 30, 'rankRangeMax' => 40],
-    ['rankName' => 'C', 'rankRangeMin' => 20, 'rankRangeMax' => 30],
-    ['rankName' => 'D', 'rankRangeMin' => 10, 'rankRangeMax' => 20],
+    ['rankName' => 'A', 'rankRangeMin' => 41, 'rankRangeMax' => 50],
+    ['rankName' => 'B', 'rankRangeMin' => 31, 'rankRangeMax' => 40],
+    ['rankName' => 'C', 'rankRangeMin' => 21, 'rankRangeMax' => 30],
+    ['rankName' => 'D', 'rankRangeMin' => 11, 'rankRangeMax' => 20],
     ['rankName' => 'E', 'rankRangeMin' => 0, 'rankRangeMax' => 10],
 ];
