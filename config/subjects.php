858bdbc1915136784384913637a1c0788b01da31
dcsyedfaraz <dcsyedfaraz@gmail.com>
2026-05-29 17:23:05 +0500

    Update subjects configuration to correct subject listings for classes 1, 3, 5, and 6, ensuring accurate representation of subjects offered. Replace 'michezo' with 'utamaduni' in class 1 and adjust subjects in class 3 for consistency.

--- config/subjects.php
+++ config/subjects.php
@@ -1,12 +1,11 @@
 <?php
 
 return [
-    '1' => ['kuhesabu', 'kusoma', 'kuandika', 'english', 'mazingira', 'michezo'],
+    '1' => ['kuhesabu', 'kusoma', 'kuandika', 'english', 'mazingira', 'utamaduni'],
     '2' => ['kuhesabu', 'kusoma', 'kuandika', 'english', 'mazingira', 'utamaduni'],
-    '3' => ['hisabati', 'kiswahili', 'sayansi', 'english', 'maadili', 'jiographia', 'smichezo'],
+    '3' => ['hisabati', 'kiswahili', 'sayansi', 'english', 'jiographia', 'historia'],
     '4' => ['hisabati', 'kiswahili', 'sayansi', 'english', 'jiographia', 'historia'],
-    '5' => ['hisabati', 'kiswahili', 'sayansi', 'english', 'jiographia', 'historia'],
-    // '5' => ['hisabati', 'kiswahili', 'sayansi', 'english', 'jamii', 'maadili', 's_kazi'],
-    '6' => ['hisabati', 'kiswahili', 'sayansi', 'english', 'jamii', 'maadili', 's_kazi'],
-    'class_default' => ['hisabati', 'kiswahili', 'sayansi', 'english', 'jamii', 'maadili'],
+    '5' => ['hisabati', 'kiswahili', 'sayansi', 'english', 'maadili', 'jamii'],
+    '6' => ['hisabati', 'kiswahili', 'sayansi', 'english', 'maadili', 'jamii'],
+    'class_default' => ['hisabati', 'kiswahili', 'sayansi', 'english', 'maadili', 'jamii'],
 ];
